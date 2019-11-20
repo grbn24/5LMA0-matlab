e7959093f3332c73bb00ce92208d9d2894aa1e20
= <g.p.n.erens@student.tue.nl>
2019-11-20 09:36:26 +0100

    added rgb image compression

--- exercise_2_2.m
+++ exercise_2_2.m
@@ -1,14 +1,24 @@
 [A] = imread('Siep_weiland.jpg');
-[A,cmap]=rgb2ind(A,1024);
-A = double(A);
-k = 100;
-[U,S,V] = svd(A);
-S_x = S;
-S_x(k+1:end,k+1:end) = 0;
-X = U*S_x*V';
-X = round(X);
-A = ind2rgb(A,cmap);
-X = ind2rgb(X,cmap);
+k = 25;
+use_index = false;
+if use_index
+    [A,cmap]=rgb2ind(A,0);
+    A = double(A);
+    [U,S,V] = svd(A);
+    X = svd_rank_red(U,S,V,k);
+    X = round(X);
+    A = ind2rgb(A,cmap);
+    X = ind2rgb(X,cmap);
+else
+    X = nan(size(A));
+    for ii = 1:size(A,3)
+        A_{ii} = double(A(:,:,ii));
+        A(:,:,ii) = uint8(A_{ii});
+        X_{ii} = svd_rank_red(A_{ii},k);
+        X(:,:,ii) = X_{ii};
+    end
+    X = uint8(X);
+end
 figure(1);
 imshow(A);
 figure(2);
--- svd_rank_red.m
+++ svd_rank_red.m
@@ -1,6 +1,19 @@
-function [A] = svd_rank_red(U,S,V,max_rank)
+function [A] = svd_rank_red(varargin)
 %SVD_RANK_RED Summary of this function goes here
 %   Detailed explanation goes here
+    switch numel(varargin)
+        case 4
+            U = varargin{1};
+            S = varargin{2};
+            V = varargin{3};
+            max_rank = varargin{4};
+        case 2
+            [U,S,V] = svd(varargin{1});
+            max_rank = varargin{2};
+            
+        otherwise
+            error("What do you want from me?? ;_;");
+    end
     S(max_rank+1:end,max_rank+1:end) = 0;
     A = U*S*V';
 end
