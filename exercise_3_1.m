583d8d8ab89c6766274ba144b784f4362c42fc75
= <g.p.n.erens@student.tue.nl>
2019-11-26 12:19:38 +0100

    finished up and polished exercise 3.1

--- exercise_3_1.m
+++ exercise_3_1.m
@@ -10,14 +10,6 @@ isobs = rank(obs)==rank(A);
 fprintf("System stable: %s\nSystem reachable: %s\nSystem observable: %s\n",logicalstr(isstable+1),logicalstr(isreach+1),logicalstr(isobs+1));
 sys = ss(A,B,C,D);
 [H,wout] = freqresp(sys);
-figure(1);
-subplot(2,2,1);
-loglog(wout,abs(H(:)))
-title('Frequency Response');
-xlabel('Frequency [rad/s]');
-ylabel('Powah');
-subplot(2,2,2);
-step(sys);
 %% Compute Gramians
 P = gram(sys,'c');
 Q = gram(sys,'o');
@@ -34,24 +26,39 @@ sys_n = ss(An,Bn,Cn,Dn);
 Pn = gram(sys_n,'c');
 Qn = gram(sys_n,'o');
 %% make reduced system
-r = 5;
+r = 6;
 Ar = An(1:r,1:r);
 Br = Bn(1:r,:);
 Cr = Cn(:,1:r);
 Dr = D-Cn*inv(An)*Bn+Cr*inv(Ar)*Br;
 sys_r = ss(Ar,Br,Cr,Dr);
-[Hr,woutr] = freqresp(sys_r);
+[Hr,woutr] = freqresp(sys_r,wout);
 %% comparison plots
-subplot(2,2,3)
+figure(1);
+subplot(2,2,1);
+loglog(wout,abs(H(:)))
+hold on;
 loglog(woutr,abs(Hr(:)));
-title(sprintf('frequency response reduce r = %d',r));
-xlabel('Frequency [rad/s]')
-ylabel('Powah')
-subplot(2,2,4)
-step(sys_r)
-title(sprintf('Step Response reduced r = %d',r));
+title('Frequency Response');
+xlabel('Frequency [rad/s]');
+ylabel('Powah');
+set(gca, 'XScale','log');
+legend(["Normal", sprintf("Reduced r = %d",r)]);
+hold off
+subplot(2,2,2)
+[y,t] = step(sys);
+plot(t,y);
+hold on
+[y,t]=step(sys_r);
+plot(t,y)
+title("Step Response");
+legend(["Normal", sprintf("Reduced r = %d",r)]);
+hold off
 %% upper bound on error
 G = tf(sys);
 G_r = tf(sys_r);
 max_e = norm(G-G_r,inf);
 fprintf("Maximum error %.5f\n",max_e);
+subplot(2,2,3:4)
+bode(G,G_r);
+legend(["Normal", sprintf("Reduced r = %d",r)]);
