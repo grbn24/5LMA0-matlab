3517aae99e9615b874b52bbbb7333cd6770d2d02
= <g.p.n.erens@student.tue.nl>
2019-12-17 11:30:09 +0100

    fixed ass 5.1 :^)

--- exercise_5_1.m
+++ exercise_5_1.m
@@ -13,14 +13,21 @@ sys_state.C = eye(2);
 [X] = lsim(sys_state,[1 0 0 0], 0:3, [0 0])'; % X should have n_states rows and n_t columns
 %% create POD basis
 [U,S,~] = svd(X);
-Ar = U(:,1:r)'*A*U(:,1:r);
-Br = U(:,1:r)'*B;
-Cr = eye(2)*U(:,1:r);
-Dr = D - eye(2)*inv(A)*B + Cr*inv(Ar)*Br;
-sys_r = ss(Ar,Br, Cr,Dr,1);
-subplot(1,3,1)
-step(sys_state)
-subplot(1,3,2)
-step(sys_r,'r')
-subplot(1,3,3)
-step(sys_state,sys_r)
+phi1 = U(:,1);
+
+
+
+
+
+% BULLSHIT
+% Ar = U(:,1:r)'*A*U(:,1:r);
+% Br = U(:,1:r)'*B;
+% Cr = eye(2)*U(:,1:r);
+% Dr = D - eye(2)*inv(A)*B + Cr*inv(Ar)*Br;
+% sys_r = ss(Ar,Br, Cr,Dr,1);
+% subplot(1,3,1)
+% step(sys_state)
+% subplot(1,3,2)
+% step(sys_r,'r')
+% subplot(1,3,3)
+% step(sys_state,sys_r)
